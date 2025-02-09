6d6e9b5fa53613de180b72acad023888a6239fea
Roger <williandmn@gmail.com>
2025-02-09 11:41:32 -0300

    feat: trata uma única requisição de valores

--- src/question2.py
+++ src/question2.py
@@ -1,3 +1,6 @@
 class Orders:
     def combine_orders(self, requests, n_max):
-        return 0
+        if requests:
+            return 1
+        else:
+            return 0
--- tests/test_question2.py
+++ tests/test_question2.py
@@ -11,3 +11,13 @@ class TestQuestion2:
 
         expected = 0
         assert result == expected
+
+    def test_single_order(self):
+        """Uma única requisição, uma única viagem"""
+        orders = [50]
+        n_max = 100
+
+        result = Orders().combine_orders(orders, n_max)
+
+        expected = 1
+        assert result == expected
