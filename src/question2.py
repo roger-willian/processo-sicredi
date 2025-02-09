39c90e47d25fc8981641b0d647a9030615629dda
Roger <williandmn@gmail.com>
2025-02-09 13:21:50 -0300

    refactor: transforma a combine_orders em tail recursive

--- src/question2.py
+++ src/question2.py
@@ -3,11 +3,16 @@ class Orders:
         result = 0
         if requests:
             sorted_requests = sorted(requests)
-            if len(sorted_requests) == 1:
-                result = 1
-            elif sorted_requests[0] + sorted_requests[-1] <= n_max:
-                result = 1 + self.combine_orders(sorted_requests[1:-1], n_max)
-            else:
-                result = 1 + self.combine_orders(sorted_requests[1:], n_max)
+            result = self._combine_orders_recursive(sorted_requests, n_max, 0)
 
         return result
+
+    def _combine_orders_recursive(self, requests, n_max, result):
+        if not requests:
+            return result
+        elif len(requests) == 1:
+            return 1 + result
+        elif requests[0] + requests[-1] <= n_max:
+            return self._combine_orders_recursive(requests[1:-1], n_max, 1 + result)
+        else:
+            return self._combine_orders_recursive(requests[1:], n_max, 1 + result)
