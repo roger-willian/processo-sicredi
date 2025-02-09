fce8e0c9502b2f56d71dd8d1306fd30007e441bd
Roger <williandmn@gmail.com>
2025-02-09 14:00:38 -0300

    refactor: otimiza a combine_orders

--- src/question2.py
+++ src/question2.py
@@ -1,18 +1,14 @@
 class Orders:
     def combine_orders(self, requests, n_max):
         result = 0
-        if requests:
-            sorted_requests = sorted(requests)
-            result = self._combine_orders_recursive(sorted_requests, n_max, 0)
+        sorted_requests = sorted(requests)
+        while sorted_requests:
+            result += 1
+            if len(sorted_requests) == 1:
+                sorted_requests = []
+            elif sorted_requests[0] + sorted_requests[-1] <= n_max:
+                sorted_requests = sorted_requests[1:-1]
+            else:
+                sorted_requests = sorted_requests[1:]
 
         return result
-
-    def _combine_orders_recursive(self, requests, n_max, result):
-        if not requests:
-            return result
-        elif len(requests) == 1:
-            return 1 + result
-        elif requests[0] + requests[-1] <= n_max:
-            return self._combine_orders_recursive(requests[1:-1], n_max, 1 + result)
-        else:
-            return self._combine_orders_recursive(requests[1:], n_max, 1 + result)
